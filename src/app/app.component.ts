c9a1619fcd8e9e6860f8a8e14e1ddf659567a4e1
rehanhussainimam <rehanhussainimam@gmail.com>
2021-10-07 23:52:39 +0530

    sadasdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PokemonModel } from './models/pokemon-model';
-import { DataService } from './services/data.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  pokemondata: PokemonModel[];
-  numberofdatatolist: number;
-  numberofpages: number;
-  currentpageindex: number;
-  filterarguments: string[];
-  offsetdata: number;
-
-  ngOnInit() {
-    this.numberofdatatolist = 5;
-    this.loadPage(this.numberofdatatolist, 1, 0);
-  }
-
-  loadPage = (numberofdatatolist, currentpageindex, offsetdata) => {
-    this.numberofdatatolist = numberofdatatolist;
-    this.currentpageindex = currentpageindex;
-    this.offsetdata = offsetdata;
-    this.DataService.getalldata(
-      this.numberofdatatolist,
-      this.offsetdata
-    ).subscribe((finalres) => {
-      this.pokemondata = finalres;
-      this.filterarguments = Object.keys(this.pokemondata[0]);
-      console.log(this.filterarguments);
-    });
-  };
-
-  previouspage() {
-    this.loadPage(
-      this.numberofdatatolist,
-      this.currentpageindex - 1,
-      this.offsetdata - Number(this.numberofdatatolist)
-    );
-  }
-
-  nextpage() {
-    this.loadPage(
-      this.numberofdatatolist,
-      this.currentpageindex + 1,
-      this.offsetdata + Number(this.numberofdatatolist)
-    );
-  }
-
-  mySelectionChange = (value) => {
-    this.currentpageindex = 0;
-    this.offsetdata = 0;
-    this.loadPage(
-      this.numberofdatatolist,
-      this.currentpageindex + 1,
-      this.offsetdata
-    );
-  };
-
-  search() {}
-
-  constructor(private DataService: DataService) {}
-}
+export class AppComponent {}
